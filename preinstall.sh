304e0da51eda58245bb164440e41a51d0d7a2227
n3xistence <maxinies00@gmail.com>
2024-03-09 14:35:16 +0100

    add first iteration of preinstall script
    installs all packages (WIP) required to set up the dotfiles

---
+++ preinstall.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+ARCH_PACKAGES=(
+  zsh
+  neovim
+  kitty
+  npm
+  nodejs
+)
+
+if [ "$2" == "-y" ]; then
+    CONFIRM="-y"
+else
+    CONFIRM=""
+fi
+
+if [ -z "$1" ]; then
+    echo "Error: No distro specified."
+    echo "Press any key to exit..."
+    read -n 1 _DISCARD
+    exit  1
+fi
+
+install_package() {
+  echo "Installing $1..."
+
+  sudo apt install $CONFIRM $1 -y
+}
+
+for pkg in "${ARCH_PACKAGES[@]}"; do
+  echo "$CONFIRM"
+  if [ "$CONFIRM" ]; then
+    install_package $pkg
+  else
+    echo -n "Install $pkg? [Y/n] : "; read -n 1 confirm_install
+    echo ""
+
+    if [ "$confirm_install" == "y" ]; then
+      install_package $pkg
+    else 
+      echo "Skipping $pkg..."
+    fi
+  fi
+done
+
+echo "Installing Oh-My-Zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
