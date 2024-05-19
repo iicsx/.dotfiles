7f5df3387b0740bddb8a77dd16473805b81bc928
n3xistence <maxinies00@gmail.com>
2024-05-19 18:36:29 +0200

    fix(screenshots): fix screenshots not being saved in directory when taking full screenshot

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE_NAME="screenshot-$(date +%F-%T).png"
-FILE_PATH="${HOME}/Pictures/screenshots/${FILE_NAME}"
+mkdir -p "${HOME}/Images/screenshots"
+FILE_PATH="${HOME}/Images/screenshots/${FILE_NAME}"
 grim -t png "${FILE_PATH}"
 notify-send 'Screenshot' -i "${FILE_PATH}" "${FILE_NAME}"
