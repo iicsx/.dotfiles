c0b207de903a2c334a06b9e2fa7cc39dc31fa400
n3xistence <maxinies00@gmail.com>
2024-08-12 15:09:48 +0200

    refactor(packer)!: restructure plugin list to form logical sections
    BREAKING!: some of these changes might break the load order if missing
    an `after` clause

--- nvim/lua/pck.lua
+++ nvim/lua/pck.lua
@@ -1,41 +1,81 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
+  -- Packer
   use('wbthomason/packer.nvim')
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
-  use('nvim-treesitter/nvim-treesitter-context', {run = ':TSUpdate'})
-  use('theprimeagen/harpoon')
-  use('mbbill/undotree')
+
+  -- git 
   use('tpope/vim-fugitive')
+
+  -- Tree Sitter
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter-context', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+  -- LSP
   use('williamboman/mason.nvim')
   use('williamboman/mason-lspconfig.nvim')
-  use('m4xshen/autoclose.nvim')
-  use('nyoom-engineering/oxocarbon.nvim')
-  use('echasnovski/mini.indentscope')
-  use('MunifTanjim/prettier.nvim')
-  use('elentok/format-on-save.nvim')
-  use('andweeb/presence.nvim')
-  use("stevearc/oil.nvim")
-  use("HiPhish/rainbow-delimiters.nvim")
-
-  use('nvim-tree/nvim-web-devicons') 
-  use('lewis6991/gitsigns.nvim') 
-  use('romgrk/barbar.nvim')
-  use('alvan/vim-closetag')
-
-  use('AlexvZyl/nordic.nvim')
-
-  use('roflolilolmao/oceanic-next.nvim')
-  use('xiyaowong/transparent.nvim')
-
-  use('Exafunction/codeium.vim')
-
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
   }
 
+  -- File Management
+  use('theprimeagen/harpoon')
+  use("stevearc/oil.nvim")
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", 
+      "MunifTanjim/nui.nvim",
+    }
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  -- Undotree
+  use('mbbill/undotree')
+
+  -- Themes
+  use('nyoom-engineering/oxocarbon.nvim')
+  use('roflolilolmao/oceanic-next.nvim')
+  use('AlexvZyl/nordic.nvim')
+  use({
+    "folke/tokyonight.nvim",
+    config = function()
+      require("tokyonight").setup({
+        style = "moon", 
+      })
+    end,
+  })
+
+  -- Visual
+  use("HiPhish/rainbow-delimiters.nvim")
+  use('echasnovski/mini.indentscope')
+  use('nvim-tree/nvim-web-devicons') 
+  use('xiyaowong/transparent.nvim')
+  use('lewis6991/gitsigns.nvim') 
+  use('romgrk/barbar.nvim')
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  -- Formatting
+  use('elentok/format-on-save.nvim')
+  use('MunifTanjim/prettier.nvim')
+
+  -- Markdown
   use({
     'MeanderingProgrammer/markdown.nvim',
     after = { 'nvim-treesitter' },
@@ -45,39 +85,21 @@ return require('packer').startup(function(use)
     end,
   })
 
-  use({
-	  "folke/tokyonight.nvim",
-	  config = function()
-		  require("tokyonight").setup({
-			  style = "moon", 
-		  })
-	  end,
-  })
+  -- Code Completion
+  use('Exafunction/codeium.vim')
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+  -- MISC -- 
+  use('m4xshen/autoclose.nvim')
+  use('andweeb/presence.nvim')
+  use('alvan/vim-closetag')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  {'neovim/nvim-lspconfig'},
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
-
-  use {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      requires = { 
-          "nvim-lua/plenary.nvim",
-          "nvim-tree/nvim-web-devicons", 
-          "MunifTanjim/nui.nvim",
-      }
-  }
-
+  -- HTTP Client
+--   use({
+--     "rest-nvim/rest.nvim",
+--     requires = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+--     config = function()
+--       require("rest-nvim").setup()
+--     end,
+--   })
+  use("diepm/vim-rest-console")
 end)
