8cca706080f2c4b589f3ec40f88110ba5b3a02b4
n3xistence <maxinies00@gmail.com>
2024-08-16 11:14:36 +0200

    refactor(plugins): fix formatting

--- nvim/after/plugin/prettier.lua
+++ nvim/after/plugin/prettier.lua
@@ -1,20 +1,19 @@
-   local prettier = require("prettier")
-   prettier.setup({
-     bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
-     filetypes = {
-       "lua",
-       "css",
-       "graphql",
-       "html",
-       "javascript",
-       "javascriptreact",
-       "json",
-       "less",
-       "markdown",
-       "scss",
-       "typescript",
-       "typescriptreact",
-       "yaml",
-     },
-   })
-
+local prettier = require("prettier")
+prettier.setup({
+  bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+  filetypes = {
+    "lua",
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+})
