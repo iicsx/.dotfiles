ce2296efc036b7889f407ad7bf92964d2c59bbda
n3xistence <maxinies00@gmail.com>
2024-09-23 09:41:09 +0200

    feat(autoUpdateNvim): add auto commands on startup to update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,4 +3,9 @@ require("set")
 require("remap")
 
 vim.cmd([[let g:netrw_bufsettings = 'noma nomod nu nobl nowrap ro']])
+
+vim.cmd([[autocmd VimEnter * lua require('packer').sync()]])
+vim.cmd([[autocmd VimEnter * TSUpdate]])
+vim.cmd([[autocmd VimEnter * MasonUpdate]])
+
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
