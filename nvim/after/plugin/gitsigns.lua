32b753009e3f2ce4ece8861b7157d16462898599
n3xistence <maxinies00@gmail.com>
2024-10-15 11:26:15 +0200

    feat(nvim): add shortcuts for manipulating hunks and toggling blame

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,1 +1,7 @@
-require("gitsigns").setup()
+local git = require("gitsigns")
+
+git.setup()
+
+vim.keymap.set("n", "<leader>gr", function() git.reset_hunk() end)
+vim.keymap.set("n", "<leader>gsh", function() git.stage_hunk() end)
+vim.keymap.set("n", "<leader>gtb", function() git.toggle_current_line_blame() end)
