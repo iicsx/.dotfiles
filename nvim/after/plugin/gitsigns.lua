9c453f9d8681c6001097628c92185360ddfbed3a
n3xistence <maxinies00@gmail.com>
2024-10-15 11:36:59 +0200

    update(nvim): shorten gitsigns keymaps

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -3,5 +3,5 @@ local git = require("gitsigns")
 git.setup()
 
 vim.keymap.set("n", "<leader>gr", function() git.reset_hunk() end)
-vim.keymap.set("n", "<leader>gsh", function() git.stage_hunk() end)
-vim.keymap.set("n", "<leader>gtb", function() git.toggle_current_line_blame() end)
+vim.keymap.set("n", "<leader>gs", function() git.stage_hunk() end)
+vim.keymap.set("n", "<leader>gb", function() git.toggle_current_line_blame() end)
