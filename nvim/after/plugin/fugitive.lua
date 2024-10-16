12e8bda935ffcf050f4aff679aea4bfa57ecf391
n3xistence <maxinies00@gmail.com>
2024-10-16 11:33:43 +0200

    update(nvim): create `starts_with` function for cleaner code

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -77,10 +77,14 @@ vim.keymap.set("n", "<leader>gc", function()
   commit()
 end)
 
+local function starts_with(str, start)
+  return str:sub(1, #start) == start
+end
+
 local function needs_linebreak(line)
-  local is_start_of_body = string.sub(line, 1, 7) == "Changes"
-  local is_end_of_body = string.sub(line, 1, 7) == "Summary"
-  local is_start_of_footer = string.sub(line, 1, 10) == "no changes"
+  local is_start_of_body = starts_with(line, "Changes")
+  local is_end_of_body = starts_with(line, "Summary")
+  local is_start_of_footer = starts_with(line, "no changes")
 
   return is_start_of_body or is_end_of_body or is_start_of_footer
 end
