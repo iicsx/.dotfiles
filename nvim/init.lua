912abfed578d591a6aa617851505e370d9956b4f
n3xistence <maxinies00@gmail.com>
2024-10-21 11:10:28 +0200

    update(nvim): remove auto-update on startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,13 +11,4 @@ vim.diagnostic.config({
   },
 })
 
-function sync_all()
-  require("packer").sync()
-
-  vim.cmd("TSUpdate")
-  vim.cmd("MasonUpdate")
-end
-
-vim.cmd([[autocmd VimEnter * lua sync_all()]])
-
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
