bf2f025a6a5c996cbe086ecb36f35280c4fc6006
n3xistence <maxinies00@gmail.com>
2024-10-22 16:25:09 +0200

    update(nvim): add bottom padding to notification windows

--- nvim/after/plugin/notify.lua
+++ nvim/after/plugin/notify.lua
@@ -5,8 +5,10 @@ require("notify").setup({
   timeout = 3000,
   top_down = false,
   on_open = function(win)
-    vim.api.nvim_win_set_config(win, {
-      -- vim.cmd [[:TransparentEnable]]
-    })
+    local height = vim.api.nvim_win_get_height(win)
+    local width = vim.api.nvim_win_get_width(win)
+    local row = vim.o.lines - height - 3
+
+    vim.api.nvim_win_set_config(win, { relative = 'editor', row = row, col = (vim.o.columns - width) / 2 })
   end,
 })
